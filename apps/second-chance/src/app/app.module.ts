fd1b8493dfe53a6d5dce55714675bef525d3df57
Julia Hardy <julia.joanna.hardy@gmail.com>
2020-08-13 17:41:07 +0200

    initial commit

--- apps/second-chance/src/app/app.module.ts
+++ apps/second-chance/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavbarComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
